cd4d5038d4aae5ac852024c17a75dc450dac48f6
Cyprien Ferreira <cyferrei@bess-f1r2s10.clusters.42paris.fr>
2024-04-02 14:10:52 +0200

    [FIX] : error on roll dice init

--- sources/character.c
+++ sources/character.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   character.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cyprien <cyprien@student.42.fr>            +#+  +:+       +#+        */
+/*   By: cyferrei <cyferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/30 03:09:35 by cyprien           #+#    #+#             */
-/*   Updated: 2024/03/30 21:09:24 by cyferrei         ###   ########.fr       */
+/*   Updated: 2024/04/02 11:40:25 by cyferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ t_character roll_stats(t_character *character)
 {
     character->ability = (rand() % 6) + 6;
     character->max_ability = character->ability;
-    character->endurance = (2 * (rand() % 6)) + 12;
+    character->endurance = (rand() % 6 + rand() % 6) + 12;
     character->max_endurance = character->endurance;
     character->luck = (rand() % 6) + 6;
     return (*character);
